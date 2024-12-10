d87337206864f40f4e941294ff7abd657b7d6c24
tianyi-xia1 <tianyi8@student.ubc.ca>
2024-12-09 22:25:38 -0800

    Add unit tests and test suite

---
+++ frenchlearning/memorization/suite.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import unittest
+
+from test_classes import TestClasses
+from test_initializer import TestInitializer
+
+
+def my_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestClasses))
+    suite.addTest(unittest.makeSuite(TestInitializer))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(my_suite())
+
+
+# In[ ]:
+
+
+
+
